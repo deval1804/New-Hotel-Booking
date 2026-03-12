2fd0aea836e0775f3b6779dd930db3bc840e6fea
Deval Darji <devaldarji93795@gmail.com>
2026-03-12 14:40:26 +0530

    fix user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 
-const useSchema = mongoose.Schema({
-    _id: {type: String, required: true},
-    username: {type: String, required: true},
-    email: {type: String, required: true},
-    image: {type: String, required: true},
-    role: {type: String, enum: ["user", "hotelOwner"], default: "user"},
-    // recentSearchedCities: [{type: String}],
-    recentSearchedCities: []
-},{timestamps: true});
+const userSchema = mongoose.Schema({
+  _id: { type: String, required: true },
+  username: { type: String },
+  email: { type: String },
+  image: { type: String },
+  role: { type: String, enum: ["user", "hotelOwner"], default: "user" },
+  recentSearchedCities: [{ type: String }]
+}, { timestamps: true });
 
-const User = mongoose.model("User", useSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
