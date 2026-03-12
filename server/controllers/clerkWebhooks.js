d606374a370a11c75bef8a0dea638597ade0b5d4
Deval Darji <devaldarji93795@gmail.com>
2026-03-12 14:20:18 +0530

    fix clerk webhook verification

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,3 +1,56 @@
+// import User from "../models/User.js";
+// import { Webhook } from "svix";
+
+// const clerkWebhooks = async (req, res) => {
+//   try {
+
+//     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+//     const headers = {
+//       "svix-id": req.headers["svix-id"],
+//       "svix-timestamp": req.headers["svix-timestamp"],
+//       "svix-signature": req.headers["svix-signature"]
+//     };
+
+//     await whook.verify(JSON.stringify(req.body), headers);
+
+//     const { data, type } = req.body;
+
+//     const userData = {
+//       _id: data.id,
+//       email: data.email_addresses[0].email_address,
+//       username: `${data.first_name || ""} ${data.last_name || ""}`,
+//       image: data.image_url,
+//     };
+
+//     switch (type) {
+
+//       case "user.created":
+//         await User.create(userData);
+//         break;
+
+//       case "user.updated":
+//         await User.findByIdAndUpdate(data.id, userData);
+//         break;
+
+//       case "user.deleted":
+//         await User.findByIdAndDelete(data.id);
+//         break;
+
+//       default:
+//         break;
+//     }
+
+//     res.json({ success: true });
+
+//   } catch (error) {
+//     console.log(error.message);
+//     res.json({ success: false, message: error.message });
+//   }
+// };
+
+// export default clerkWebhooks;
+
 import User from "../models/User.js";
 import { Webhook } from "svix";
 
@@ -12,15 +65,16 @@ const clerkWebhooks = async (req, res) => {
       "svix-signature": req.headers["svix-signature"]
     };
 
-    await whook.verify(JSON.stringify(req.body), headers);
+    const payload = await whook.verify(req.body, headers);
 
-    const { data, type } = req.body;
+    const { data, type } = payload;
 
     const userData = {
       _id: data.id,
       email: data.email_addresses[0].email_address,
       username: `${data.first_name || ""} ${data.last_name || ""}`,
       image: data.image_url,
+      recentSearchedCities: []
     };
 
     switch (type) {
@@ -44,8 +98,8 @@ const clerkWebhooks = async (req, res) => {
     res.json({ success: true });
 
   } catch (error) {
-    console.log(error.message);
-    res.json({ success: false, message: error.message });
+    console.log(error);
+    res.json({ success: false });
   }
 };
 
