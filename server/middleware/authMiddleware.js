e935e2b9297dc325491ba91c0e299b9f31edad4d
Deval Darji <devaldarji93795@gmail.com>
2026-03-17 11:15:15 +0530

    authMiddleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -14,25 +14,17 @@ import User from "../models/User.js";
 // }
 
 export const protect = async (req, res, next) => {
-    try {
 
-        const { userId } = req.auth;
+    console.log("AUTH DATA:", req.auth);
 
-        if (!userId) {
-            return res.json({ success: false, message: "not authenticated" });
-        }
+    const { userId } = req.auth;
 
-        const user = await User.findById(userId);
-
-        if (!user) {
-            return res.json({ success: false, message: "User not found" });
-        }
-
-        req.user = user;
-
-        next();
-
-    } catch (error) {
-        res.json({ success: false, message: error.message });
+    if (!userId) {
+        return res.json({ success: false, message: "not authenticated" });
     }
-};
+
+    const user = await User.findById(userId);
+
+    req.user = user;
+    next();
+}
