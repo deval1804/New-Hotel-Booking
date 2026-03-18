bc1d0174092ba8f2b91a337c3735cb6067df36b4
Deval Darji <devaldarji93795@gmail.com>
2026-03-18 11:10:13 +0530

    fix auth middleware user creation

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,14 +1,43 @@
+// import User from "../models/User.js";
+
+// //Middleware to check if the user is authenticated
+// export const protect = async (req, res, next) => {
+//     const { userId } = req.auth;
+//     if (!userId) {
+//          res.json({ success: false, message: "not authenticated" })
+//     }
+//     else{
+//         const user = await User.findById(userId);
+//         req.user = user;
+//         next();
+//     }
+// }
+
 import User from "../models/User.js";
 
-//Middleware to check if the user is authenticated
 export const protect = async (req, res, next) => {
-    const { userId } = req.auth;
-    if (!userId) {
-         res.json({ success: false, message: "not authenticated" })
-    }
-    else{
-        const user = await User.findById(userId);
+    try {
+
+        const { userId } = req.auth;
+
+        if (!userId) {
+            return res.json({ success: false, message: "Not authenticated" });
+        }
+
+        let user = await User.findById(userId);
+
+        // ⭐ agar user DB me nahi mila to create kar do
+        if (!user) {
+            user = await User.create({
+                _id: userId
+            });
+        }
+
         req.user = user;
+
         next();
+
+    } catch (error) {
+        res.json({ success: false, message: error.message });
     }
-}
+};
