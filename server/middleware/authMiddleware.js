6edc8b6544ed9e339f367872d2d0bea3c500e62e
Deval Darji <devaldarji93795@gmail.com>
2026-03-25 15:15:29 +0530

    first commit

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,10 +1,8 @@
 import User from "../models/User.js";
 
-//Middleware to check if the user is authenticated
 export const protect = async (req, res, next) => {
-    console.log("AUTH DATA:", req.auth);
 
-    const { userId } = req.auth || {};
+    const { userId } = req.auth();   // ⭐ yaha () lagana hai
 
     if (!userId) {
         return res.json({ success: false, message: "not authenticated" });
@@ -12,5 +10,6 @@ export const protect = async (req, res, next) => {
 
     const user = await User.findById(userId);
     req.user = user;
+
     next();
 };
